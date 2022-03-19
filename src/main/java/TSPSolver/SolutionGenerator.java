d3531282055822ab7ee6bd5105bdc00e50865d41
Wojtekm <123w456m@gmail.com>
2022-03-19 16:29:30 +0100

    Added 2opt and kRandom, fixed TSPLoader

---
+++ src/main/java/TSPSolver/SolutionGenerator.java
@@ -0,0 +1,23 @@
+package TSPSolver;
+
+import java.util.List;
+
+public abstract class SolutionGenerator {
+
+    public abstract List<Integer> solve(double[][] distanceTable);
+
+    public double objectiveFunction(List<Integer> pointList, double[][] distanceMatrix) {
+        double objectiveFunctionValue = 0.0;
+
+        int size = pointList.size() - 1;
+
+        for(int i = 0; i < size; i++) {
+            objectiveFunctionValue += distanceMatrix[pointList.get(i)][pointList.get(i + 1)];
+        }
+
+        objectiveFunctionValue += distanceMatrix[pointList.get(size)][pointList.get(0)];
+
+        return objectiveFunctionValue;
+    }
+}
+
