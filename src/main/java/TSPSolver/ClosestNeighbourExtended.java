e5b2ada22ffc8b4ba2c862520f16ced31e35b877
protosto <48259030+protosto@users.noreply.github.com>
2022-03-22 17:23:16 +0100

    Closest neighbour methods

---
+++ src/main/java/TSPSolver/ClosestNeighbourExtended.java
@@ -0,0 +1,27 @@
+package TSPSolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtendedClosestNeighbourMethod extends SolutionGenerator{
+    @Override
+    public List<Integer> solve(double[][] distanceMatrix) {
+
+        List<Integer> pointList = new ArrayList<>();
+
+        double minObjective = objectiveFunction( ClosestNeighbourMethod.solveFor(distanceMatrix, 0), distanceMatrix);
+        pointList = ClosestNeighbourMethod.solveFor(distanceMatrix, 0);
+
+        for( int i = 1; i < distanceMatrix.length; i++ ){
+            if( objectiveFunction( ClosestNeighbourMethod.solveFor(distanceMatrix, i), distanceMatrix) < minObjective ){
+                minObjective = objectiveFunction( ClosestNeighbourMethod.solveFor(distanceMatrix, i), distanceMatrix);
+                pointList = ClosestNeighbourMethod.solveFor(distanceMatrix, 0);
+            }
+        }
+
+        return pointList;
+    }
+}
+
+
+
